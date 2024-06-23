0b8d533f2d32f126fa1500419f9e7f49cfbd6fd1
Siddharth Srivastava <siddharthsrivastava76@gmail.com>
2024-06-23 11:08:03 +0100

    Fixed bug with compute_message_right

--- defining-factors.py
+++ defining-factors.py
@@ -38,7 +38,7 @@ class Variable:
 
     def belief_update(self):
         '''
-        Belief update for a variable node is simply the product of all received messages, Ortiz (2003) eq. 2.13
+        Belief update for a variable node is simply the product of all received messages, Ortiz (2023) eq. 2.13
         '''
         eta, lmbda = torch.zeros_like(self.eta), torch.zeros_like(self.lmbda)
 
@@ -61,7 +61,7 @@ class Variable:
             if fid == -1: continue
 
             try:
-                # Eqn. 2.21 from Ortiz (2003), which is var -> factor message
+                # Eqn. 2.21 from Ortiz (2023), which is var -> factor message
                 in_eta, in_lmbda = self.inbox[fid].eta, self.inbox[fid].lmbda
                 out_eta, out_lmbda = self.eta - in_eta, self.lmbda - in_lmbda
 
@@ -91,6 +91,42 @@ class Parameter:
 
     def get_sigma(self): return self.sigma
 
+    def belief_update(self):
+        '''
+        Belief update for a variable node is simply the product of all received messages, Ortiz (2023) eq. 2.13
+        '''
+        eta, lmbda = torch.zeros_like(self.eta), torch.zeros_like(self.lmbda)
+
+        # Consume messages from the inbox and update belief
+        for _, message in self.inbox.items():
+            eta += message.eta
+            lmbda += message.lmbda
+
+        if lmbda == 0. : print('We Hebben Een Serieus Probleem ')
+
+        self.eta, self.lmbda = eta, lmbda
+
+        self.sigma = torch.linalg.inv(self.lmbda)
+        self.mu = self.sigma @ self.eta
+
+    def compute_messages(self):
+        self.belief_update()
+
+        for fid in self.connected_factors:
+            if fid == -1: continue
+
+            try:
+                # Eqn. 2.21 from Ortiz (2023), which is var -> factor message
+                in_eta, in_lmbda = self.inbox[fid].eta, self.inbox[fid].lmbda
+                out_eta, out_lmbda = self.eta - in_eta, self.lmbda - in_lmbda
+
+                factor_nodes[fid].inbox[self.var_id] = Gaussian(out_eta, out_lmbda)
+            except KeyError:
+                # print(f'Landed here, sending {self.eta} and {self.lmbda} to {fid}')
+                factor_nodes[fid].inbox[self.var_id] = Gaussian(self.eta, self.lmbda)
+
+        self.inbox = {}
+
 
 class ObservationFactor:
     def __init__(self, f_id, var_id, z, lmbda_in) -> None:
@@ -117,7 +153,7 @@ class ObservationFactor:
         r = self.z - var_nodes[self.var_id].get_mu()
         M = torch.sqrt(r * self.lmbda_in[0,0] * r)
 
-        # Eqn. 3.20 from Ortiz (2003)
+        # Eqn. 3.20 from Ortiz (2023)
         if M > self.N_sigma:
             kR = (2*self.N_sigma / M) - (self.N_sigma**2 / M**2)
             return kR
@@ -145,7 +181,7 @@ class DynamicsFactor:
         self.k = k
 
     def linearise(self):
-        # Bunch of computations to linearise this factor (Ortiz (2003) eqns. 2.46 and 2.47)
+        # Bunch of computations to linearise this factor (Ortiz (2023) eqns. 2.46 and 2.47)
         Et_mu, _ = canonical_to_moments(self.inbox[self.Et_id].eta, self.inbox[self.Et_id].lmbda)
         Etp_mu, _ = canonical_to_moments(self.inbox[self.Etp_id].eta, self.inbox[self.Etp_id].lmbda)
 
@@ -174,66 +210,69 @@ class DynamicsFactor:
         r = self.z - self.h
         M = torch.sqrt(r * self.lmbda_in[0,0] * r)
 
-        # Eqn. 3.20 from Ortiz (2003)
+        # Eqn. 3.20 from Ortiz (2023)
         if M > self.N_sigma:
             kR = (2*self.N_sigma / M) - (self.N_sigma**2 / M**2)
             return kR
 
         return 1
 
+    # Message f to Etp -> eta = [eta_Et, eta_Etp], lambda = [[l_EtEt, l_EtEtp], [l_EtpEt, l_EtpEtp]]
     def _compute_message_going_right(self):
         self.linearise()
 
         in_eta, in_lmbda = self.inbox[self.Et_id].eta, self.inbox[self.Et_id].lmbda
         kR = self.huber()
 
+        # All of this is eqn 8 from 'Learning in Deep Factor Graphs with Gaussian Belief Propagation'
+        # We set \i to j
         eta_here, lambda_here = self.eta.clone(), self.lmbda.clone()
-
         eta_here[0] = self.eta[0] + in_eta
         lambda_here[0,0] = self.lmbda[0,0] + in_lmbda
 
         eta_here *= kR
         lambda_here *= kR
 
-        eta_b, eta_a = eta_here
-        lambda_aa, lambda_ab = lambda_here[1]
-        lambda_ba, lambda_bb = lambda_here[0]
+        eta_j, eta_i = eta_here
+        lambda_jj, lambda_ji = lambda_here[0]
+        lambda_ij, lambda_ii = lambda_here[1]
 
-        lambda_ab_lambda_bb_inv = lambda_ab / lambda_bb
+        lambda_ij_lambda_jj_inv = lambda_ij / lambda_jj
 
-        eta = eta_a - lambda_ab_lambda_bb_inv * eta_b
-        lmbda = lambda_aa - lambda_ab_lambda_bb_inv * lambda_ba
+        eta = eta_i - lambda_ij_lambda_jj_inv * eta_j
+        lmbda = lambda_ii - lambda_ij_lambda_jj_inv * lambda_ji
 
         return eta, lmbda
 
+    # Message f to Et -> eta = [eta_Et, eta_Etp], lambda = [[l_EtEt, l_EtEtp], [l_EtpEt, l_EtpEtp]]
     def _compute_message_going_left(self):
         self.linearise()
 
         in_eta, in_lmbda = self.inbox[self.Etp_id].eta, self.inbox[self.Etp_id].lmbda
         kR = self.huber()
 
+        # All of this is eqn 8 from 'Learning in Deep Factor Graphs with Gaussian Belief Propagation'
         eta_here, lambda_here = self.eta.clone(), self.lmbda.clone()
-
         eta_here[1] = self.eta[1] + in_eta
         lambda_here[1,1] = self.lmbda[1,1] + in_lmbda
 
         eta_here *= kR
         lambda_here *= kR
 
-        eta_a, eta_b = eta_here
-        lambda_aa, lambda_ab = lambda_here[0]
-        lambda_ba, lambda_bb = lambda_here[1]
+        eta_i, eta_j = eta_here
+        lambda_ii, lambda_ij = lambda_here[0]
+        lambda_ji, lambda_jj = lambda_here[1]
 
-        lambda_ab_lambda_bb_inv = lambda_ab / lambda_bb
+        lambda_ij_lambda_jj_inv = lambda_ij / lambda_jj
 
-        eta = eta_a - lambda_ab_lambda_bb_inv * eta_b
-        lmbda = lambda_aa - lambda_ab_lambda_bb_inv * lambda_ba
+        eta = eta_i - lambda_ij_lambda_jj_inv * eta_j
+        lmbda = lambda_ii - lambda_ij_lambda_jj_inv * lambda_ji
 
         return eta, lmbda
 
     def compute_messages(self):
         var_nodes[self.Et_id].inbox[self.f_id]  = Gaussian(*self._compute_message_going_left())
-        # var_nodes[self.Etp_id].inbox[self.f_id] = Gaussian(*self._compute_message_going_right())
+        var_nodes[self.Etp_id].inbox[self.f_id] = Gaussian(*self._compute_message_going_right())
 
         self.inbox = {}
 
@@ -251,52 +290,50 @@ def update_dynamics_factor(key):
 
 if __name__ == "__main__":
 
-    sigma_obs = 1e-2
-    sigma_dynamics = 1e-3
+    sigma_obs = 1e0
+    sigma_dynamics = 1e-2
 
     signal = np.load('E_synthetic_kdot5_Pdot2.npy')[50:250].astype(np.float32)
     # signal = [1., 2., 3.]
     t = torch.arange(0, len(signal), 1)
     dt = 0.01
-    sigma_obs_values = [1e2, 1e1, 1e0, 1e-1, 1e-2]
-    sigma_dynamics_values = [1e2, 1e1, 1e0, 1e-1, 1e-2]
+    # sigma_obs_values = [1e1]
+    # sigma_dynamics_values = [1e-2]
 
-    fig, axs = plt.subplots(5, 5, figsize=(30, 30))
+    fig, axs = plt.subplots(1, 1, figsize=(5, 5))
 
-    for a, sigma_obs in enumerate(sigma_obs_values):
-        for b, sigma_dynamics in enumerate(sigma_dynamics_values):
-            ax = axs[a,b]
-            for k in [0., .2, .5, 1., 5.]:
-                var_nodes = {}
-                factor_nodes = {}
+    ax = axs
+    for k in [0., .2, .5, 1., 5.]:
+        var_nodes = {}
+        factor_nodes = {}
 
-                for iters in [20]:
-                    for i in range(len(t)):
-                        var_nodes[i] = Variable(i, torch.tensor([[0.]]), torch.tensor([[0.]]), -1 if i == 0 else (i-1, i), -1 if i+1 == len(t) else (i,i+1), i)
-                        factor_nodes[i] = ObservationFactor(i, i, signal[i], torch.tensor([[sigma_obs ** -2]]))
+        for iters in [50]:
+            for i in range(len(t)):
+                var_nodes[i] = Variable(i, torch.tensor([[0.]]), torch.tensor([[0.]]), -1 if i == 0 else (i-1, i), -1 if i+1 == len(t) else (i,i+1), i)
+                factor_nodes[i] = ObservationFactor(i, i, signal[i], torch.tensor([[sigma_obs ** -2]]))
 
-                    for i in range(len(t)):
-                        if i + 1 < len(t):
-                            factor_nodes[(i, i+1)] = DynamicsFactor(i, i+1, torch.tensor([[sigma_dynamics ** -2]]), (i, i+1), k)
+            for i in range(len(t)):
+                if i + 1 < len(t):
+                    factor_nodes[(i, i+1)] = DynamicsFactor(i, i+1, torch.tensor([[sigma_dynamics ** -2]]), (i, i+1), k)
 
-                    for i in range(iters):
-                        # print(f'---- Iteration {i} ----')
-                        for key in factor_nodes:
-                            update_observational_factor(key)
+            for i in range(iters):
+                # print(f'---- Iteration {i} ----')
+                for key in factor_nodes:
+                    update_observational_factor(key)
 
-                        for key in var_nodes:
-                            update_variable_belief(key)
+                for key in var_nodes:
+                    update_variable_belief(key)
 
-                        for key in factor_nodes:
-                            update_dynamics_factor(key)
+                for key in factor_nodes:
+                    update_dynamics_factor(key)
 
-                    recons_signal = torch.tensor([var_nodes[key].get_mu() for key in var_nodes])
-                    ax.plot(recons_signal, label = f'k = {k}')
+            recons_signal = torch.tensor([var_nodes[key].get_mu() for key in var_nodes])
+            ax.plot(recons_signal, label = f'k = {k}')
 
-                ax.set_title(rf'$\sigma_o = {sigma_obs}, \sigma_d = {sigma_dynamics}$')
+        ax.set_title(rf'$\sigma_o = {sigma_obs}, \sigma_d = {sigma_dynamics}$')
 
-            ax.plot(signal, label = 'Noisy Signal')
-            ax.legend()
+    ax.plot(signal, label = 'Noisy Signal')
+    ax.legend()
 
     # plt.tight_layout()
     # plt.savefig('temp4.pdf', dpi=600, bbox_inches='tight')
